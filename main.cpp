3a4375ba203656041b15ef317af88a298e3d1548
SushiCannibale <sushi.cannibale@gmail.com>
2024-04-07 17:06:51 -0400

    merge

--- main.cpp
+++ main.cpp
@@ -3,45 +3,79 @@
 
 #include "sort.hpp"
 
+template <class T>
+void show_array(int nb_vals, int nb_funcs, std::string func_names[], T* data) {
+    /* Front padding (8-spaces format of nb_vals) */
+    std::cout << "        | ";
+    for (int i = 0; i < nb_funcs-1; i++) {
+        std::cout << func_names[i] << " | ";
+    }
+
+    std::cout << func_names[nb_funcs-1] << std::endl;
+    fprintf(stdout, "%8d", nb_vals);
+
+    /* Prints function names */
+    for (int i = 0; i < nb_funcs-1; i++) {
+        for (int j = 0; j < func_names[i].length(); j++) {
+            std::cout << " ";
+        }
+        std::cout << " | ";
+    }
+    for (int j = 0; j < func_names[nb_funcs-1].length(); j++) {
+        std::cout << " ";
+    }
+
+    /* Print stats */
+    for (int i = 0; i < nb_funcs; i++) {
+        for (int j = 0; j < func_names[i].length()/2; j++) {
+            std::cout << " ";
+        }
+        std::cout << data[i];
+        for (int j = 0; j < func_names[i].length()/2; j++) {
+            std::cout << " ";
+        }
+    }
+}
+
 int main(int argc, char** argv) {
-    /* Sizes of arrays to sort */
-    int nb_samp = 3;
-    int samples[] = { 500, 5000, 50000 };
-
-    /* Functions to compare ! */
+    /* Functions to compare */
     int nb_funcs = 3;
-    void (*funcs[])(std::vector<int>, int*, int*) = { insertion_sort, merge_sort, quick_sort };
+    void (*funcs[nb_funcs])(std::vector<int>, int*, int*) = { insertion_sort, merge_sort, quick_sort };
+    std::string func_names[nb_funcs] = { "Insertion Sort", "Merge Sort", "Quick Sort" };
 
+    /* Stats */
     time_t start, end;
-    double timediffs[nb_samp * nb_funcs];
-    int nb_swaps[nb_samp * nb_funcs];
-    int nb_cmps[nb_samp * nb_funcs];
+    double timediffs[nb_funcs];
+    int nb_swaps[nb_funcs];
+    int nb_cmps[nb_funcs];
 
-    /* For each stats, (timediff, nb_swaps, nb_comps) : */
     //          fn1       fn2        fn3       ...
     //   500     X         X          X
-    //  5000     X         X          X
-    // 50000     X         X          X
     //   ...
 
-    for (int j = 0; j < nb_samp; j++) {
-        for (int i = 0; i < nb_funcs; i++) {
-            std::vector<int> array;
-            
-            /* Fills the array */
-            for (int k = 0; k < samples[j]; k++) {
-                array.push_back(rand());
-            }
+    int user_nb_vals; // TODO: fills the user input 'user_nb_vals'
 
-            /* Runs the function & store the stats */
-            time(&start);
-            funcs[i](array, &nb_swaps[j*nb_funcs + i], &nb_cmps[j*nb_funcs + i]);
-            time(&end);
-
-            timediffs[j*nb_funcs + i] = difftime(end, start);
+    for (int i = 0; i < nb_funcs; i++) {
+        std::vector<int> array;
+        
+        /* Fills the array */
+        for (int k = 0; k < user_nb_vals; k++) {
+            array.push_back(rand());
         }
+
+        /* Runs the function & store the stats */
+        time(&start);
+        funcs[i](array, &nb_swaps[i], &nb_cmps[i]);
+        time(&end);
+
+        timediffs[i] = difftime(end, start);
     }
 
     /* Display the arrays */
-    for (int i = 0; )
+    std::cout << "Timestamps" << std::endl;
+    show_array<double>(user_nb_vals, nb_funcs, func_names, timediffs);
+    std::cout << "Number of swaps" << std::endl;
+    show_array<int>(user_nb_vals, nb_funcs, func_names, nb_swaps);
+    std::cout << "Number of comparisons" << std::endl;
+    show_array<int>(user_nb_vals, nb_funcs, func_names, nb_cmps);
 }
--- sort.hpp
+++ sort.hpp
@@ -4,7 +4,6 @@
 #include <ctime>
 #include <vector>
 
-/* out_* -> can be NULL if unwanted */
 /* It is up to function to count its swaps & comparisons number */
 void insertion_sort(std::vector<int> array, int* out_nb_swap, int* out_nb_cmp);
 void merge_sort(std::vector<int> array, int* out_nb_swap, int* out_nb_cmp);
